94471a84abfa3018379bb2d85dbeb24a87f312f3
renildomarcio <krsolucoesweb@gmail.com>
2023-11-22 12:49:25 -0300

    Update 1.0.0

---
+++ config.js
@@ -0,0 +1,22 @@
+const config = {
+    // Main Application URL
+    'websiteUrl' : 'https://launcher.elitedarodagem.online',
+
+    // Application Name
+    'appName' : 'Launcher Elite da Rodagem',
+
+    // Application window width and height
+    'width' : 1280,
+    'height' : 800,
+    'minWidth' : 1280,
+    'minHeight' : 800,
+
+    // Hide elements by ID
+    'hideElementsId' : [],
+
+    // Hide elements by Class
+    'hideElementsClass' : [],
+
+}
+
+module.exports = config
---
+++ main.js
@@ -0,0 +1,247 @@
+const {app, BrowserWindow, nativeTheme, ipcMain, Menu, Tray, nativeImage, dialog} = require('electron')
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
+const path = require('path')
+const appConfig = require('./config')
+
+let mainWindow;
+let tray = null;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: appConfig['width'],
+    height: appConfig['height'],
+    minWidth: appConfig['minWidth'],
+    minHeight: appConfig['minHeight'],
+    icon: path.join(__dirname, 'favicon.ico'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: true
+    }
+  })
+
+  mainWindow.setMenu(null);
+
+  mainWindow.maximize();
+
+  loadWebContent()
+
+  const trayIcon = nativeImage.createFromPath(path.join(__dirname, 'public/logo_tray.png'));
+  tray = new Tray(trayIcon);
+
+  const contextMenu = Menu.buildFromTemplate([
+    { 
+      label: appConfig['appName'] +' v' + app.getVersion(),
+      icon: nativeImage.createFromPath(__dirname + '/public/logo_tray.png').resize({ width: 16 }),
+      click: () => mainWindow.show() 
+    },
+    { type: 'separator' },
+    { 
+      label: 'Sobre',
+      icon: nativeImage.createFromPath(__dirname + '/public/sobre.png').resize({ width: 16 }),  
+      click: () => { require('./main')("about") } 
+    },
+    { type: 'separator' },
+    { 
+      label: 'Atualização',
+      icon: nativeImage.createFromPath(__dirname + '/public/update.png').resize({ width: 16 }), 
+      click() { autoUpdater.checkForUpdates() } 
+    },
+    { type: 'separator' },
+    { 
+      label: 'Abrir', 
+      icon: nativeImage.createFromPath(__dirname + '/public/maxi.png').resize({ width: 16 }),
+      click: () => mainWindow.show() 
+    },
+    { type: 'separator' },
+    { 
+      label: 'Minimizar', 
+      icon: nativeImage.createFromPath(__dirname + '/public/mini.png').resize({ width: 16 }),
+      click: () => mainWindow.minimize() 
+    },
+    { type: 'separator' },
+    { 
+      label: 'Fechar',
+      icon: nativeImage.createFromPath(__dirname + '/public/close.png').resize({ width: 16 }), 
+      click: () => app.quit() 
+    }
+  ]);
+
+  tray.setToolTip(appConfig['appName']);
+  tray.setContextMenu(contextMenu);
+
+  mainWindow.webContents.once('dom-ready', () => {
+    mainWindow.show()
+    setActivity()
+    tray.on('click', () => {
+        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+    })
+    autoUpdater.checkForUpdates()
+  })
+
+  mainWindow.on('minimize', (event) => {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  mainWindow.on('close', (event) => {
+    if (!app.isQuitting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+
+  app.on('before-quit', () => {
+    app.isQuitting = true;
+  });
+
+  app.on('web-contents-created', (event, contents) => {
+    contents.on('new-window', (event, navigationUrl) => {
+      event.preventDefault();
+      require('electron').shell.openExternal(navigationUrl);
+    });
+  });
+}
+
+function loadWebContent() {
+  mainWindow.loadFile(path.join(__dirname, 'public/loading.html'))
+
+  let wc = mainWindow.webContents
+
+  wc.once('did-finish-load'  ,  () => {
+    mainWindow.loadURL(appConfig['websiteUrl'])
+  })
+
+  wc.on('did-fail-provisional-load', (error, code)=> {
+    mainWindow.loadFile(path.join(__dirname, 'public/offline.html'))
+  })
+}
+
+ipcMain.on('online-status-changed', (event, status) => {
+  if(status == true) { loadWebContent() }
+})
+
+module.exports = (pageId) => {
+  if(pageId === 'home') {
+    loadWebContent()
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `public/${pageId}.html`))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+function setActivity() {
+
+/*  const client = require('discord-rich-presence')('1165497253855772694')
+  client.on("error", _ => true);
+
+  // discordClient
+  client.on('connected', () => {
+      startTimestamp = new Date();
+      client.updatePresence({
+          state: "Community",
+          startTimestamp,
+          largeImageKey: "logo",
+          instance: true,
+          buttons: [
+              { "label": "Website", "url": "https://renildomarcio.com.br" },
+              { "label": "Forum", "url": "https://community.renildomarcio.com.br" }
+          ]
+      });
+
+      setInterval(() => { 
+          client.updatePresence({
+              state: "Community",
+              startTimestamp,
+              largeImageKey: 'logo',
+              largeImageText: "Community",
+              smallImageKey: 'online',
+              smallImageText: "Você esta online!",
+              instance: true,
+              buttons: [
+                { "label": "Website", "url": "https://renildomarcio.com.br" },
+                { "label": "Forum", "url": "https://community.renildomarcio.com.br" }
+              ]
+          });
+      }, 35500);
+  });*/
+}
+
+
+function handleUpdateChecking() {
+  log.log("Checking for updates.");
+}
+
+function handleUpdateAvailable(info) {
+  log.log("Update available.");
+}
+
+function handleDownloadProgress(progressObj) {
+  const message = `Downloading update. Speed: ${progressObj.bytesPerSecond} - ${~~progressObj.percent}% [${progressObj.transferred}/${progressObj.total}]`;
+
+  log.log(message);
+
+  mainWindow.webContents.executeJavaScript(`
+    Swal.fire({
+      title: 'Baixando atualização',
+      html: '${message}',
+      allowOutsideClick: false,
+      onBeforeOpen: () => {
+          Swal.showLoading();
+      }
+    });
+  `);
+}
+
+function handleUpdateError(err) {
+  log.log(`Update check failed: ${err.toString()}`);
+}
+
+function handleUpdateNotAvailable(info) {
+  log.log("Update not available.");
+
+  mainWindow.webContents.executeJavaScript(`
+    Swal.fire({
+      title: 'Atualizações',
+      html: 'Não há atualizações disponíveis.',
+      icon: 'error'
+    });
+  `);
+}
+
+function handleUpdateDownloaded(info) {
+  mainWindow.webContents.executeJavaScript(`
+    Swal.fire({
+      title: 'Reiniciando o aplicativo',
+      html: 'Aguente firme, reiniciando o aplicativo para atualização!',
+      allowOutsideClick: false,
+      onBeforeOpen: () => {
+          Swal.showLoading();
+      }
+    });
+  `);
+
+  autoUpdater.quitAndInstall();
+}
+
+autoUpdater.on('checking-for-update', handleUpdateChecking);
+autoUpdater.on('update-available', handleUpdateAvailable);
+autoUpdater.on('download-progress', handleDownloadProgress);
+autoUpdater.on('error', handleUpdateError);
+autoUpdater.on('update-not-available', handleUpdateNotAvailable);
+autoUpdater.on('update-downloaded', handleUpdateDownloaded);
+
