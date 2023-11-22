f3ca2b1ec9c621cacc5b4643e9600e1ada6d08e4
renildomarcio <krsolucoesweb@gmail.com>
2023-11-22 12:59:57 -0300

    Update build.js

--- build.js
+++ build.js
@@ -13,7 +13,7 @@ class Index {
                 appId: preductname,
                 productName: preductname,
                 icon: "./build/icon.ico",
-                copyright: "Copyright © 2015-2023 Community - Dev by RenildoMarcio",
+                copyright: "Copyright © 2015-2023 Launcher EDR - Dev by RenildoMarcio",
                 artifactName: "${productName}-${os}-${arch}.${ext}",
                 files: ["**/*", "package.json", "LICENSE.md"],
                 directories: { "output": "dist" },
